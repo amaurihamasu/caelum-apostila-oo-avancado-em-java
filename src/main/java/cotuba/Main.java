48659db5df84e50c81bb1c544d7054adbe9f5a79
Amauri Hamasu <am.hamasu@gmail.com>
2021-08-01 22:16:28 -0300

    Exercício: separando a Aplicação do Controller no Cotuba - questão 4

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -6,15 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
-
-        Path diretorioDosMD = opcoesCLI.getDiretorioDosMD();
-        String formato = opcoesCLI.getFormato();
-        Path arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
-        boolean modoVerboso = opcoesCLI.isModoVerboso();
+        Path diretorioDosMD;
+        String formato;
+        Path arquivoDeSaida;
+        boolean modoVerboso = false;
 
         try {
 
+            LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
+
+            diretorioDosMD = opcoesCLI.getDiretorioDosMD();
+            formato = opcoesCLI.getFormato();
+            arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
+            modoVerboso = opcoesCLI.isModoVerboso();
+
+            
             Cotuba cotuba = new Cotuba();
             cotuba.executa(formato, diretorioDosMD, arquivoDeSaida);
 
