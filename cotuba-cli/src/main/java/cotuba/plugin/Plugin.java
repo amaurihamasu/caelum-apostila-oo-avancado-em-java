98f2fc9c2f1560c87c9631f43f1e635a420fd2c5
Amauri Hamasu <am.hamasu@gmail.com>
2021-08-08 16:35:47 -0300

    Exercício: testando o plugin de tema & rollback exercicios opcionais

--- cotuba-cli/src/main/java/cotuba/plugin/Plugin.java
+++ cotuba-cli/src/main/java/cotuba/plugin/Plugin.java
@@ -1,30 +1,21 @@
 package cotuba.plugin;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 public interface Plugin {
-
-    // Exercício: carregando o service provider
-    // (desafio) O método estático load, da classe ServiceLoader,
-    // vasculha o Classpath em busca de implementações da SPI,
-    // o que é um processo lento. Otimize o código armazenando a instância de ServiceLoader retornada.
-    static final ServiceLoader<Plugin> LOADER = ServiceLoader.load(Plugin.class);
 
     String cssDoTema();
 
     static List<String> listaDeTemas() {
 
-        // Exercício: carregando o service provider
-        // (opcional) No método listaDeTemas da classe Plugin, use recursos do Java 8 como
-        // Lambdas e Streams para trabalhar com o ServiceLoader.
-        List<String> temas =
-                LOADER
-                    .stream()
-                    .map(loaderPlugin -> loaderPlugin.get().cssDoTema())
-                    .collect(Collectors.toList());
-
+        List<String> temas = new ArrayList<>();
+        ServiceLoader<Plugin> loader = ServiceLoader.load(Plugin.class);
+        for (Plugin plugin : loader) {
+            String css = plugin.cssDoTema();
+            temas.add(css);
+        }
         return temas;
     }
 
